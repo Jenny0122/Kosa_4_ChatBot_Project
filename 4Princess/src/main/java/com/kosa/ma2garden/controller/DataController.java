f3e69d5fd619f75fdddc09c47b51540c681ca1ce
안선정 <asj4459@naver.com>
2022-03-15 22:00:38 +0900

    2021-03-15 Vue 연동

--- 4Princess/src/main/java/com/kosa/ma2garden/controller/DataController.java
+++ 4Princess/src/main/java/com/kosa/ma2garden/controller/DataController.java
@@ -1,7 +1,7 @@
 package com.kosa.ma2garden.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DataController {
 }
--- 4Princess/src/main/java/com/kosa/ma2garden/scheduler/SchedulerService.java
+++ 4Princess/src/main/java/com/kosa/ma2garden/scheduler/SchedulerService.java
@@ -1,22 +1,8 @@
 package com.kosa.ma2garden.scheduler;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import com.kosa.ma2garden.entity.User;
-import com.kosa.ma2garden.repository.UserRepository;
 
 @Service
 public class SchedulerService {
 
-	@Autowired
-	UserRepository userRepository;
-
-	@Scheduled(fixedDelay = 1000000)
-	public void test() {
-		for (User users : userRepository.findAll()) {
-			System.out.println(users);
-		}
-	}
 }
