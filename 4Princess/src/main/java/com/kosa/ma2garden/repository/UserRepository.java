ed30fcca11cd3780a4d38f25dc6aa57c9804ee0d
Jenny0122 <asj4459@naver.com>
2022-04-07 18:07:00 +0900

    User 삭제

--- 4Princess/src/main/java/com/kosa/ma2garden/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kosa.ma2garden.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kosa.ma2garden.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
