f5fb396667827727470d152d89c6204c31da575b
dlwl0319 <dlwl0319@gmail.com>
2022-04-11 22:59:33 +0900

    [service] created chatbot service

---
+++ 4Princess/src/main/java/com/kosa/ma2garden/service/ChatBotService.java
@@ -0,0 +1,36 @@
+package com.kosa.ma2garden.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatBotService {
+
+	public String getAnswer(String query) throws InterruptedException, IOException {
+		String command = "/Users/easy/anaconda3/envs/md/bin/python";
+		String arg1 = "/Users/easy/wisenut/4princess/chatbotAPI/ChatBotAPI.py";
+
+		ProcessBuilder builder = new ProcessBuilder(command, arg1, query);
+		Process process = builder.start();
+		int exitVal = process.waitFor();
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), "utf-8"));
+
+		String line;
+		String result = null;
+		
+		while ((line = br.readLine()) != null) {
+//			System.out.println(">>> " + line);
+			result += line;
+		}
+
+		if (exitVal != 0) {
+//			System.out.println("비정상 종료");
+			return null;
+		} else {
+			return result;
+		}
+	}
+}
