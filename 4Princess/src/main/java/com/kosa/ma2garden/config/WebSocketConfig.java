ef61486bf17b24eee49aba437a62e7b5d61d8a4b
Jenny0122 <asj4459@naver.com>
2022-04-15 19:00:51 +0900

    2022-04-15-19:00

--- 4Princess/src/main/java/com/kosa/ma2garden/config/WebSocketConfig.java
+++ 4Princess/src/main/java/com/kosa/ma2garden/config/WebSocketConfig.java
@@ -7,39 +7,31 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-//
-//@Configuration
-//@EnableWebSocketMessageBroker
-//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
-//
-//
-//	@Override // 클라이언트가 메시지를 구독할 endpoint를 정의합니다.
-//	public void configureMessageBroker(MessageBrokerRegistry config) {
-//		config.enableSimpleBroker("/send");
-//	}
-//
-//	@Override // connection을 맺을때 CORS 허용합니다.
-//	public void registerStompEndpoints(StompEndpointRegistry registry) {
-//		registry.addEndpoint("/")
-//				.setAllowedOriginPatterns("*")
-//				.withSockJS();
-//	}
-//
-//	@Override
-//	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-////	@Autowired
-////	private EchoHandler echoHandler;
-////	@Override
-////	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-////		registry.addHandler(echoHandler, "/echo")
-////				.setAllowedOrigins("*")
-////				.withSockJS()
-////				.setInterceptors(new HttpSessionHandshakeInterceptor());
-////
-////	}
-//
-//}
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSocketConfigurer {
+
+   @Override // 클라이언트가 메시지를 구독할 endpoint를 정의합니다.
+   public void configureMessageBroker(MessageBrokerRegistry config) {
+      config.enableSimpleBroker("/send");
+   }
+
+   @Override // connection을 맺을때 CORS 허용합니다.
+   public void registerStompEndpoints(StompEndpointRegistry registry) {
+      registry.addEndpoint("/chat")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
+   }
+
+   private final WebSocketHandler webSocketHandler = new WebSocketHandler();
+
+   @Override
+   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+      registry.addHandler(webSocketHandler, "/chat")
+            .setAllowedOrigins("*")
+            .withSockJS();
+
+   }
+
+}
---
+++ 4Princess/src/main/java/com/kosa/ma2garden/config/WebSocketHandler.java
@@ -0,0 +1,23 @@
+package com.kosa.ma2garden.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+   @Override
+   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+      String payload = message.getPayload();
+      log.info("payload : {}", payload);
+
+      TextMessage initialGreeting = new TextMessage("Welcome to Ma2Garden Chat Server!");
+      session.sendMessage(initialGreeting);
+   }
+
+}
